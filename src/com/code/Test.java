bea09bba78aa750b79aa2dbcbcc52b46fc920dfc
Zelin Su <1005719242@qq.com>
2017-12-12 12:14:55 +0800

    Create Test.java

---
+++ src/com/code/Test.java
@@ -0,0 +1,12 @@
+package com.code;
+
+/**
+ * 测试类，运行此类执行计算器窗口
+ * @author NorwegianForest
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String args[]){
+        MainFrame f = new MainFrame();
+    }
+}
